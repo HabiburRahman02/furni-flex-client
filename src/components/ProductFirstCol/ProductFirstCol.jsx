5829a47d60aec2c71431bc9613c9a08cbbb65749
Habibur Rahman <habibur88454@gmail.com>
2024-09-06 02:39:39 +0600

    Products Container leftside nav

---
+++ src/components/ProductFirstCol/ProductFirstCol.jsx
@@ -0,0 +1,12 @@
+
+const ProductFirstCol = () => {
+    return (
+        <div className="p-6 space-y-4 text-xl font-medium text[#717171]">
+            <button className="bg-[#202020] w-full text-white font-semibold py-4 rounded-lg text-start pl-4"> Rocking Chair</button>
+            <button className="border-y p-5 w-full text-start pl-4">Side Chair</button>
+            <button className="w-full text-start pl-4">Lounge Chair</button>
+        </div>
+    );
+};
+
+export default ProductFirstCol;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import ProductCard from "../ProductCard/ProductCard";
+import ProductFirstCol from "../ProductFirstCol/ProductFirstCol";
 
 
 const Products = () => {
@@ -9,7 +10,7 @@ const Products = () => {
         <div className="max-w-[1200px] mx-auto">
             <div className="md:flex gap-8 space-y-8 md:space-y-0">
                 <div className="md:w-1/4 border-[#F1F1F1] border-r ">
-                    df
+                    <ProductFirstCol></ProductFirstCol>
                 </div>
                 <div className="md:w-3/4 grid grid-cols-1 md:grid-cols-3 gap-8">
                     {
